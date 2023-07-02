1c3a3d087d2943380de802cbb676a3146346f16d
fredagbona <fredagbona@gmail.com>
2023-07-02 17:37:25 +0100

    [ADD] Initial files of app

---
+++ Readme.md
@@ -0,0 +1,17 @@
+# GITHUB USER SEARCHING WEB APP
+
+## Description
+
+Github User Searching is web app that uses Github API for provides some informations about user searched such as his full name, github username, github profile link, number of its repositories...
+
+## How it works
+
+When you enter a username in search bar, if this user exists, the result will be a user card with user's informations. If user don't exists, the result wil be a error message  and if you click search button within empty search bar, the result will be a list of 40 defaults users on Github with informations of each of them
+
+### Link to test 
+
+[Visit here](https://githubsearchuser.netlify.app)
+
+
+Feel free to fork this project or clone it to make improvements especially on the design. I will appreciate it.
+
---
+++ assets/scripts/script.js
@@ -0,0 +1,134 @@
+function printValue() {
+  let searchUser = document.getElementById("search_bar").value;
+  const accessToken = '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';
+  const headers = new Headers();
+  headers.append('Authorization', `token ${accessToken}`);
+
+  function createModal(data) {
+    var modal = document.createElement("div");
+    modal.classList.add("modal");
+  
+    var modalContent = document.createElement("div");
+    modalContent.classList.add("modal-content");
+  
+    var closeBtn = document.createElement("span");
+    closeBtn.classList.add("close");
+    closeBtn.innerHTML = "&times;";
+    closeBtn.addEventListener("click", closeModal);
+  
+    var heading = document.createElement("h3");
+    heading.textContent = "Single User Infos";
+  
+    var paragraph = document.createElement("p");
+    paragraph.innerHTML = "Name : " + data.name + "  <br> Login : " + data.login + " <br> Repos Count : " + data.public_repos + " ";
+  
+    var image = document.createElement("img");
+    image.src = data.avatar_url;
+    image.width = 100;
+    image.style.borderRadius = '100%';
+  
+    var link = document.createElement("a");
+    link.textContent = "Github Profile Link";
+    link.href = data.html_url;
+    link.target = "_blank";
+  
+    modalContent.appendChild(closeBtn);
+    modalContent.appendChild(heading);
+    modalContent.appendChild(image);
+    modalContent.appendChild(paragraph);
+    modalContent.appendChild(link);
+  
+    modal.appendChild(modalContent);
+  
+    return modal;
+  }
+  
+
+  function openModal(modal) {
+    document.body.appendChild(modal);
+    modal.style.display = "block";
+    document.addEventListener("click", outsideModalClick);
+  }
+
+  function closeModal() {
+    var modal = document.querySelector(".modal");
+    document.body.removeChild(modal);
+    document.removeEventListener("click", outsideModalClick);
+  }
+
+  function outsideModalClick(event) {
+    var modal = document.querySelector(".modal");
+    if (event.target === modal) {
+      closeModal();
+    }
+  }
+
+  if (searchUser == "") {
+    document.getElementById("error_message").innerHTML = "";
+    document.getElementById("printres").innerHTML = "";
+    const perPage = 40; // Number of users per page
+    const page = 1; // Page Number
+
+    fetch(`https://api.github.com/users?per_page=${perPage}&page=${page}`, { headers })
+      .then(response => response.json())
+      .then(data => {
+        for (let single_user in data) {
+          let div = document.createElement("div");
+          div.classList.add('results');
+          const image = document.createElement("img");
+          image.src = data[single_user].avatar_url;
+          image.width = 150;
+          image.style.borderRadius = '100%';
+          const username = document.createElement("h3");
+          username.innerHTML = "Username : " + data[single_user].login;
+          div.appendChild(image);
+          div.appendChild(username);
+          document.getElementById("printres").appendChild(div);
+
+          div.addEventListener("click", () => {
+            fetchUserData(data[single_user].login, headers)
+              .then(data => {
+                var modal = createModal(data);
+                openModal(modal);
+              });
+          });
+        }
+      });
+  } else {
+    fetch(`https://api.github.com/users/${searchUser}`, { headers })
+      .then(response => response.json())
+      .then(data => {
+        if (data.message == "Not Found") {
+          document.getElementById("printres").innerHTML = "";
+          document.getElementById("error_message").innerHTML = "The search did not return any data";
+        } else {
+          document.getElementById("error_message").innerHTML = "";
+          document.getElementById("printres").innerHTML = "";
+          let div = document.createElement("div");
+          div.classList.add('results');
+          const image = document.createElement("img");
+          image.src = data.avatar_url;
+          image.width = 100;
+          image.style.borderRadius = '100%';
+          const username = document.createElement("h3");
+          username.innerHTML = "Username : " + data.login;
+          div.appendChild(image);
+          div.appendChild(username);
+          document.getElementById("printres").appendChild(div);
+
+          div.addEventListener("click", () => {
+            var modal = createModal(data);
+            openModal(modal);
+          });
+        }
+      });
+  }
+}
+
+function fetchUserData(searchUser, headers) {
+  return fetch(`https://api.github.com/users/${searchUser}`, { headers })
+    .then(response => response.json())
+    .then(data => {
+      return data;
+    });
+}
